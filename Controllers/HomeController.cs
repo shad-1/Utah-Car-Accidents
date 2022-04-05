47d7a5fa8b220428bc99046cd4c6573c8de8aa60
Jerron <jerron.byu@gmail.com>
2022-04-04 21:36:58 -0600

    YOLO

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -11,6 +11,7 @@ namespace YeetCarAccidents.Controllers
 {
     public class HomeController : Controller
     {
+
         private readonly ILogger<HomeController> _logger;
 
         public HomeController(ILogger<HomeController> logger)
@@ -27,11 +28,12 @@ namespace YeetCarAccidents.Controllers
         {
             return View();
         }
-
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        //COOKIE SECTION
+        
     }
 }
