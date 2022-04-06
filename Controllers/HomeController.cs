286154837eef59e9aca9a11e7dcf298bfc9f7033
atopham <90280882+atopham@users.noreply.github.com>
2022-04-05 18:55:50 -0600

    merge Shad's wrk with mine

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -60,6 +60,56 @@ namespace YeetCarAccidents.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
         //COOKIE SECTION
+
+
+        public IActionResult Admin()
+        {
+            var crashes = _repo.Crashes.Include("Location").ToList();
+            return View(crashes);
+        }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            ViewBag.Locations = _repo.Locations.ToList();
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Crash c)
+        {
+            if (ModelState.IsValid)
+            {
+                var crashes = _repo.Crashes.ToList();
+                var max = 0;
+                foreach(Crash crash in crashes)
+                {
+                    if (max < crash.CrashId)
+                    {
+                        max = crash.CrashId;
+                    }
+                }
+                c.CrashId = max + 1;
+                _repo.Add(c);
+                return View("Confirmation", c);
+            }
+            else
+            {
+                ViewBag.Locations = _repo.Locations.ToList();
+                return View();
+            }
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int crashid)
+        {
+            _repo.Update(c);
+            return RedirectToAction("Admin");
+        }
+        [HttpPost]
+        public IActionResult Edit(Crash c)
+        {
+
+        }
         
     }
 }
